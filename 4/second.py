fbd921fe9db5d0bd3604bf8da8d837f4373d5274
Lennard Wiecking <lennard.wiecking@uni-oldenburg.de>
2022-12-04 08:21:15 +0100

    Added day 4

---
+++ 4/second.py
@@ -0,0 +1,22 @@
+def counter(inp):
+    pair_one = inp.split(",")[0]
+    pair_two = inp.split(",")[1]
+
+    pair_one_lower = int(pair_one.split("-")[0])
+    pair_one_upper = int(pair_one.split("-")[1])
+
+    pair_two_lower = int(pair_two.split("-")[0])
+    pair_two_upper = int(pair_two.split("-")[1])
+
+    if pair_two_lower <= pair_one_lower <= pair_two_upper or \
+        pair_one_lower <= pair_two_lower <= pair_one_upper:
+        return 1
+    else:
+        return 0
+
+
+p = 0
+with open("ranges", "r") as d:
+    for inp in d.readlines():
+        p += counter(inp)
+print(p)
