a3fd99406a980ec84e80a09eb25f2bcfbc2aae1b
Lennard Wiecking <lennard.wiecking@uni-oldenburg.de>
2022-12-12 01:31:31 +0100

    Added day 10 the first

---
+++ 10/second.py
@@ -0,0 +1,37 @@
+def get_summed_cycle_strength(input):
+
+    cycle_measure_numbers = [20, 60, 100, 140, 180, 220]
+    x = 1
+    res = 0
+    round = 0
+    num = 0
+
+    for inp in input:
+        num += 1
+        round += 1
+
+        if round in cycle_measure_numbers:
+            res += x * round
+            print("Cycle measure number! ", round, x, x * round, res)
+
+        if inp != 0:
+            round += 1
+
+            if round in cycle_measure_numbers:
+                res += x * round
+                print("Cycle measure number! ", round, x, x * round, res)
+
+            x += inp
+
+    return res
+
+
+input = []
+with open("input", "r") as d:
+    for inp in d.readlines():
+        if inp.startswith("addx "):
+            input.append(int(inp.replace("addx ", "")))
+        else:
+            input.append(0)
+
+print(get_summed_cycle_strength(input))
