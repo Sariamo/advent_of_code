ead13b19cb6e63dcb224e3c76e16fa42cef1cb22
Lennard Wiecking <lennard.wiecking@uni-oldenburg.de>
2022-12-15 03:36:59 +0100

    Added two tries

--- 12/first.py
+++ 12/first.py
@@ -1,10 +1,46 @@
-import random
+def get_cost(matrix, p):
+    dest = []
+    for i, row in enumerate(matrix):
+        if "S" in matrix[i]:
+            dest = [i, row.index("S")]
+            break
+    return abs(dest[0] - p[0]) + abs(dest[1] - p[1])
+
+
+def get_field_with_minimum_cost(matrix, queue):
+    min_cost = None
+    min_cost_field = None
+    for q in queue:
+        cost = get_cost(matrix, q)
+        if min_cost is None or cost < min_cost:
+            min_cost = cost
+            min_cost_field = q
+    return min_cost_field
+
+
+def is_reachable(matrix, a, b):
+    alphabet = ['S', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'E']
+
+    return 0 <= b[0] <= len(matrix) and 0 <= b[1] <= len(matrix[0]) \
+        and alphabet.index(matrix[a[0][a[1]]]) + 1 >= alphabet.index(matrix[b[0][b[1]]])
+
+
+def get_reachable_neighbors(matrix, a):
+    reachable_neighbors = []
+    if is_reachable(matrix, a, [a[0] + 1, a[1]]):
+        reachable_neighbors.append([a[0] + 1, a[1]])
+    if is_reachable(matrix, a, [a[0] - 1, a[1]]):
+        reachable_neighbors.append([a[0] - 1, a[1]])
+    if is_reachable(matrix, a, [a[0], a[1] - 1]):
+        reachable_neighbors.append([a[0], a[1] - 1])
+    if is_reachable(matrix, a, [a[0], a[1] + 1]):
+        reachable_neighbors.append([a[0], a[1] + 1])
+
+    return reachable_neighbors
 
 
 def get_fewest_steps(matrix):
-    alphabet = ['S', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
     row, column = 0, 0
 
     for r, height_x in enumerate(matrix):
@@ -13,48 +49,61 @@ def get_fewest_steps(matrix):
                 row, column = r, c
                 break
 
-    used_fields = [[row, column]]
+    # Dijkstra
+    start = [row, column]
+    used_fields = []
+    queue = [get_reachable_neighbors(matrix, start)]
     steps = 0
 
-    while matrix[row][column] != "E":
+    while queue:
 
         steps += 1
 
-        possible_directions = []
-        if row > 0 and matrix[row - 1][column] == "E" or row < len(matrix) - 1 and matrix[row + 1][column] == "E" \
-                or column > 0 and matrix[row][column - 1] == "E" or column < len(matrix[row]) - 1 and matrix[row][column + 1] == "E":
-            return steps
+        min_cost_neighbor = get_field_with_minimum_cost(matrix, queue)
+        new_fields = get_reachable_neighbors(matrix, min_cost_neighbor)
 
-        if row > 0 and [row - 1, column] not in used_fields and \
-                alphabet.index(matrix[row - 1][column]) == alphabet.index(matrix[row][column]) or \
-                len(alphabet) > alphabet.index(matrix[row][column]) + 1 == alphabet.index(matrix[row - 1][column]):
-            possible_directions.append("up")
-        if row < len(matrix) and [row + 1, column] not in used_fields and \
-                alphabet.index(matrix[row + 1][column]) == alphabet.index(matrix[row][column]) or \
-                len(alphabet) > alphabet.index(matrix[row][column]) + 1 == alphabet.index(matrix[row + 1][column]):
-            possible_directions.append("down")
-        if column > 0 and [row, column - 1] not in used_fields and \
-                alphabet.index(matrix[row][column - 1]) == alphabet.index(matrix[row][column]) or \
-                len(alphabet) > alphabet.index(matrix[row][column - 1]) + 1 == alphabet.index(matrix[row][column]):
-            possible_directions.append("left")
-        if column < len(matrix[row]) and [row, column + 1] not in used_fields and \
-                alphabet.index(matrix[row][column + 1]) == alphabet.index(matrix[row][column]) or \
-                len(alphabet) > alphabet.index(matrix[row][column + 1]) + 1 == alphabet.index(matrix[row][column]):
-            possible_directions.append("right")
-        print(possible_directions)
-        if not possible_directions:
-            break
+    # Own heuristic
 
-        chosen_dir = random.choice(possible_directions)
-        if chosen_dir == "up":
-            row, column = row - 1, column
-        if chosen_dir == "down":
-            row, column = row + 1, column
-        if chosen_dir == "left":
-            row, column = row, column - 1
-        if chosen_dir == "right":
-            row, column = row, column + 1
-        used_fields.append([row, column])
+    # while matrix[row][column] != "E":
+    #
+    #     steps += 1
+    #
+    #     possible_directions = []
+    #     if row > 0 and matrix[row - 1][column] == "E" or row < len(matrix) - 1 and matrix[row + 1][column] == "E" \
+    #             or column > 0 and matrix[row][column - 1] == "E" or column < len(matrix[row]) - 1 and \
+    #             matrix[row][column + 1] == "E":
+    #         return steps
+
+    #     if row > 0 and [row - 1, column] not in used_fields and \
+    #             alphabet.index(matrix[row - 1][column]) == alphabet.index(matrix[row][column]) or \
+    #             len(alphabet) > alphabet.index(matrix[row][column]) + 1 == alphabet.index(matrix[row - 1][column]):
+    #         possible_directions.append("up")
+    #     if row < len(matrix) and [row + 1, column] not in used_fields and \
+    #             alphabet.index(matrix[row + 1][column]) == alphabet.index(matrix[row][column]) or \
+    #             len(alphabet) > alphabet.index(matrix[row][column]) + 1 == alphabet.index(matrix[row + 1][column]):
+    #         possible_directions.append("down")
+    #     if column > 0 and [row, column - 1] not in used_fields and \
+    #             alphabet.index(matrix[row][column - 1]) == alphabet.index(matrix[row][column]) or \
+    #             len(alphabet) > alphabet.index(matrix[row][column - 1]) + 1 == alphabet.index(matrix[row][column]):
+    #         possible_directions.append("left")
+    #     if column < len(matrix[row]) and [row, column + 1] not in used_fields and \
+    #             alphabet.index(matrix[row][column + 1]) == alphabet.index(matrix[row][column]) or \
+    #             len(alphabet) > alphabet.index(matrix[row][column + 1]) + 1 == alphabet.index(matrix[row][column]):
+    #         possible_directions.append("right")
+    #     print(possible_directions)
+    #     if not possible_directions:
+    #         break
+    #
+    #     chosen_dir = random.choice(possible_directions)
+    #     if chosen_dir == "up":
+    #         row, column = row - 1, column
+    #     if chosen_dir == "down":
+    #         row, column = row + 1, column
+    #     if chosen_dir == "left":
+    #         row, column = row, column - 1
+    #     if chosen_dir == "right":
+    #         row, column = row, column + 1
+    #     used_fields.append([row, column])
 
 
 input = []
--- 14/first.py
+++ 14/first.py
@@ -1,24 +1,109 @@
 def fall(input):
-    pass
+    x_offset = 463
+    fall_ctr = 0
+    x_pos = 500 - x_offset
+    while True:
+
+        if input[fall_ctr][x_pos] == '.':
+            input[fall_ctr][x_pos] = '+'
+            if fall_ctr > 0:
+                input[fall_ctr - 1][x_pos] = '.'
+            fall_ctr += 1
+            continue
+
+        if input[fall_ctr][x_pos] == '#' or input[fall_ctr][x_pos] == '+':
+
+            if input[fall_ctr][x_pos - 1] == '.':
+                x_pos -= 1
+                input[fall_ctr][x_pos - 1] = '+'
+                input[fall_ctr - 1][x_pos] = "."
+                fall_ctr += 1
+                continue
+
+            elif input[fall_ctr][x_pos + 1] == '.':
+                x_pos += 1
+                input[fall_ctr][x_pos + 1] = '+'
+                input[fall_ctr - 1][x_pos] = "."
+                fall_ctr += 1
+                continue
+
+            else:
+                input[fall_ctr - 1][x_pos] = '+'
+                break
+
+    return input
 
 
 def get_first_one_to_fall_out(input):
-    map = []
+
+    sand_count = 0
+    while True:
+        input = fall(input)
+
+        for inp in input:
+            print(str(inp).replace("'", "").replace("[", "").replace("]", "").replace(",", ""))
+        print()
+        sand_count += 1
+        print(sand_count)
 
 
-falling = []
+def convert_to_matrix(rocks):
+    x_offset = 463
+    y_offset = 14
+
+    rock_matrix = [["." for _ in range(158)] for _ in range(72)]
+
+    for rock_complex in rocks:
+
+        for rock_row in range(len(rock_complex) - 1):
+
+            rock_coords = rock_complex[rock_row]
+            rock_coords_next = rock_complex[rock_row + 1]
+
+            if rock_coords_next[0] - rock_coords[0] < 0:
+
+                for i in range(rock_coords[0] - rock_coords_next[0]):
+                    rock_matrix[rock_coords_next[0] + i - x_offset][rock_coords[1] - y_offset] = "#"
+
+            if rock_coords_next[0] - rock_coords[0] > 0:
+
+                for i in range(rock_coords_next[0] - rock_coords[0]):
+                    rock_matrix[rock_coords[0] + i - x_offset][rock_coords[1] - y_offset] = "#"
+
+            if rock_coords_next[1] - rock_coords[1] < 0:
+
+                for i in range(rock_coords[1] - rock_coords_next[1]):
+                    rock_matrix[rock_coords_next[0] - x_offset][rock_coords_next[1] + i - y_offset] = "#"
+
+            if rock_coords_next[1] - rock_coords[1] > 0:
+
+                for i in range(rock_coords_next[1] - rock_coords[1]):
+                    rock_matrix[rock_coords[0] - x_offset][rock_coords[1] + i - y_offset] = "#"
+
+    new_rock_matrix = [["." for _ in range(len(rock_matrix))] for _ in range(len(rock_matrix[0]))]
+
+    for i in range(len(rock_matrix)):
+        for j in range(len(rock_matrix[i])):
+            new_rock_matrix[j][i] = rock_matrix[i][j]
+
+    return new_rock_matrix
+
+
+rocks = []
 with open("input", "r") as d:
     for inp in d.readlines():
-        falling_i = inp.split(" -> ")
-        falling_properly_formatted = []
-        for single_falling in falling_i:
-            falling_properly_formatted.append(single_falling.split(","))
-        falling_int = []
-        falling.append(falling_properly_formatted)
+        rock_i = inp.split(" -> ")
+        rocks_properly_formatted = []
+        for single_falling in rock_i:
+            rocks_properly_formatted.append(single_falling.split(","))
+        rocks_int = []
+        rocks.append(rocks_properly_formatted)
 
-for fall_path in falling:
-    for fall in fall_path:
-        for i, fall_axis in enumerate(fall):
-            fall[i] = int(fall_axis.replace("\n", ""))
+for rock in rocks:
+    for ro in rock:
+        for i, rock_axis in enumerate(ro):
+            ro[i] = int(rock_axis.replace("\n", ""))
 
-print(get_first_one_to_fall_out(input()))
+
+rock_matrix = convert_to_matrix(rocks)
+print(get_first_one_to_fall_out(rock_matrix))
